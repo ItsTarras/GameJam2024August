53b147a8a68c99bfccd8b251e8161c08666f13ba
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-08-17 23:16:41 +1200

    Make machine timing configurable

--- Assets/Scripts/HitDetection/HitDetect.cs
+++ Assets/Scripts/HitDetection/HitDetect.cs
@@ -8,10 +8,22 @@ public class HitDetect : MonoBehaviour
     [SerializeField] AudioSource hitConfirmSoundEffect;
     [SerializeField] public KeyCode key;
 
+    // Beat matching config
+    [SerializeField] int cooldown; // hit the key every x beats
+
+    // radius around the exact timing where your input still counts
+    // (eg within forgiveness beats before or after perfect still counts)
+    [SerializeField] float forgiveness;
+
+
     void Update()
     {
-        //Test hit detection. For every second beat, if you time it properly, a sound effect will play. (The hitConfirmSoundEffect variable.
-        if (Input.GetKeyDown(key) && (soundManager.songPositionInBeats % 2 > 1.8f || soundManager.songPositionInBeats % 2 < 0.2f))
+        // For every `cooldown` beat, if you time it properly,
+        // a sound effect will play. (The hitConfirmSoundEffect variable.)
+        if (Input.GetKeyDown(key) && (
+            soundManager.songPositionInBeats % cooldown > cooldown - forgiveness ||
+            soundManager.songPositionInBeats % cooldown < forgiveness
+        ))
         {
             hitConfirmSoundEffect.Play();
         }
