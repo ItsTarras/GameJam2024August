4e48bce94be40547ebf9fabd10cda6077bc1d85e
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-08-18 15:16:16 +1200

    red circle animated

--- Assets/Scripts/InteractionPrompt.cs
+++ Assets/Scripts/InteractionPrompt.cs
@@ -6,6 +6,8 @@ public class InteractionPrompt : MonoBehaviour
     public HitDetect parentMachine;
     [SerializeField] Transform worldSpaceAnchor;
     private Camera MainCamera;
+    [SerializeField] SpriteRenderer redCircle;
+    private float circleScale = 1f;
 
     void Start()
     {
@@ -17,6 +19,9 @@ public class InteractionPrompt : MonoBehaviour
 
     void Update()
     {
+        // Shrink red circle
+        redCircle.material.SetFloat("_Scale", Mathf.PingPong(Time.time, circleScale));
+
         // Match button position to machine
         Vector3 screenPos = MainCamera.WorldToScreenPoint(worldSpaceAnchor.position);
         transform.position = screenPos;
