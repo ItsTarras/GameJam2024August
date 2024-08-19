ad89beb8ed0bb69659ecd8ca3849e23de30519c5
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-08-20 11:15:13 +1200

    import music

--- Assets/Scripts/MusicDetector/SoundManager.cs
+++ Assets/Scripts/MusicDetector/SoundManager.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 
 [RequireComponent(typeof(AudioSource))]
 public class SoundManager : MonoBehaviour
--- Assets/Scripts/MusicDetector/returnBPM.cs
+++ Assets/Scripts/MusicDetector/returnBPM.cs
@@ -1,15 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class returnBPM : MonoBehaviour
 {
-
     public AudioClip targetClip;
     private float bpm = 0;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         bpm = UniBpmAnalyzer.AnalyzeBpm(targetClip);
@@ -19,12 +14,6 @@ public class returnBPM : MonoBehaviour
     public float getBPM()
     {
         return bpm;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
 
